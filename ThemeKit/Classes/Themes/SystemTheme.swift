61b235c42c0f8b1b0f1016ba66fefdc7d8306086
tgprojet <88248239+tgprojet@users.noreply.github.com>
2022-10-10 11:33:58 -0600

    Update SystemTheme.swift

--- ThemeKit/Classes/Themes/SystemTheme.swift
+++ ThemeKit/Classes/Themes/SystemTheme.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 class SystemTheme: ITheme {
-    var hudBlurStyle: UIBlurEffect.Style { UITraitCollection.current.userInterfaceStyle == .dark ? .dark : .extraLight }
+    var hudBlurStyle: UIBlurEffect.Style { UITraitCollection.default.userInterfaceStyle == .dark ? .dark : .extraLight }
     var keyboardAppearance: UIKeyboardAppearance { .default }
     var statusBarStyle: UIStatusBarStyle { .default }
 
